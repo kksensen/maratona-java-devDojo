f8b38df84d0b526e684f1fde8f7c3faedb08ac72
kksensen <kksensentuts@gmail.com>
2026-06-07 22:32:19 -0300

    feat: classes abstratas java

---
+++ src/academy/devdojo/maratonajava/javacore/lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.lclassesabstratas.test;
+
+import academy.devdojo.maratonajava.javacore.lclassesabstratas.dominio.Desenvolvedor;
+import academy.devdojo.maratonajava.javacore.lclassesabstratas.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.lclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+
+        Gerente gerente = new Gerente("Nami", 5000);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Touya", 12000);
+        System.out.println(gerente);
+        System.out.println(desenvolvedor);
+        gerente.imprime();
+        desenvolvedor.imprime();
+    }
+}
